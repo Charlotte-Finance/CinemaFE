58754fc73d85c2647d4093122765a000c1de37bf
charlotte <charlotte.finance@etu.univ-lyon1.fr>
2021-11-18 20:29:30 +0100

    ✅ Update Model according to new version of database

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,9 +1,11 @@
 class Character {
+  int id;
   int actorId;
   int movieId;
   String name;
 
   Character({
+    required this.id,
     required this.actorId,
     required this.movieId,
     required this.name,
@@ -11,16 +13,18 @@ class Character {
 
   factory Character.fromJson(Map<String, dynamic> json) {
     return Character(
-      actorId: json['key']['actorId'],
-      movieId: json['key']['movieId'],
+      id: json['id'],
+      actorId: json['actorId'],
+      movieId: json['movieId'],
       name: json['name'],
     );
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['key']['actorId'] = actorId;
-    data['key']['movieId'] = movieId;
+    data['id'] = id;
+    data['actorId'] = actorId;
+    data['movieId'] = movieId;
     data['name'] = name;
     return data;
   }
